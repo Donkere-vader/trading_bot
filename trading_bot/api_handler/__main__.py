f84285f8fef38bba4c52761725ae993bef6576f0
Donkere-vader <donkere.v@gmail.com>
2020-11-21 14:19:22 +0100

    Added finance.yahoo web scraper

--- trading_bot/api_handler/__main__.py
+++ trading_bot/api_handler/__main__.py
@@ -5,9 +5,11 @@ class APIHandler:
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def get_interesting_stocks(self):
-        # Make yahoo financing call
-        pass
+    def get_interesting_stocks(self, amount):
+        """ Get the {amount} best gainers according to finacne.yahoo.com """
+        req = requests.get("https://finance.yahoo.com/gainers")
+        soup = BeautifulSoup(req.content, features="html.parser")
+        return [i.contents[0] for i in soup.findAll("a", {"class": "Fw(600) C($linkColor)"})[:amount]]
 
     def stock_info(self, stock):
         # Make alpha vantage call
