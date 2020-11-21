b95c6f9c04b945ceecf4b375000df6dc08571371
Donkere-vader <donkere.v@gmail.com>
2020-11-21 12:25:02 +0100

    First part of API Handler

--- trading_bot/api_handler/__main__.py
+++ trading_bot/api_handler/__main__.py
@@ -1,5 +1,14 @@
-
+import requests
+from bs4 import BeautifulSoup
 
 class APIHandler:
-    def __init__(self):
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def get_interesting_stocks(self):
+        # Make yahoo financing call
+        pass
+
+    def stock_info(self, stock):
+        # Make alpha vantage call
         pass
