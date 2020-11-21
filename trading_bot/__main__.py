c64e7ace73ce04e7caed2bda2d6dbb8d258d3296
Donkere-vader <donkere.v@gmail.com>
2020-11-21 17:04:45 +0100

    Dont webscraper finace.yahoo everytime only once every 10 minutes

--- trading_bot/__main__.py
+++ trading_bot/__main__.py
@@ -1,6 +1,8 @@
 from .console import Console
 from .api_handler import APIHandler
 from .models import Stock
+from datetime import datetime as dt
+from datetime import timedelta
 import json
 # from time import sleep
 
@@ -17,8 +19,13 @@ class Bot:
     def __init__(self, api_key):
         self.api_handler = APIHandler(api_key)
         self.console = Console(LOGO, log_file_name='trading_bot')
+        self._interesting_stocks = {
+            "last_checked": dt(1970, 1, 1, 0, 0, 0),
+            "stocks": []
+        }
 
         self.console.log(self.owned_stocks)
+
 
     @property
     def owned_stocks(self) -> list:
@@ -26,11 +33,13 @@ class Bot:
 
     @property
     def interesting_stocks(self) -> list:
-        interesting_stocks = self.owned_stocks.copy()
-        if len(interesting_stocks) < 5:
-            interesting_stocks += self.api_handler.get_interesting_stocks(
-                amount=5 - len(interesting_stocks)
-                )
+        if dt.now() - self._interesting_stocks['last_checked'] > timedelta(minutes=10):
+            self.console.log("Webscraping call")
+            interesting_stocks = self.api_handler.get_interesting_stocks(amount=5)
+            self._interesting_stocks['stocks'] = interesting_stocks
+            self._interesting_stocks['last_checked'] = dt.now()
+        else:
+            interesting_stocks = self._interesting_stocks['stocks']
         
         return interesting_stocks
 
